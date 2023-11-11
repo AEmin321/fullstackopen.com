cd7aa312befb890e74bcb49de4e1f3546c40324f
hamini <ahmademin321@gmail.com>
2023-11-11 22:42:19 +0300

    Exercise 1.11 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,23 +2,23 @@ import { useState } from 'react'
 
 const Title=({title})=><h1>{title}</h1>
 const Button=({onClick,text})=><button onClick={onClick}>{text}</button>
-const StatisticLine=({value,text})=><p>{text} {value}</p>
+const StatisticLine=({value,text})=><tr><td>{text}</td><td>{value}</td></tr>
 const Statistics=({good,neutral,bad})=>{
   const total=good+neutral+bad
   const average = (good + (bad * -1)) / total
   const positive = good / total * 100 
   if (total===0) {
-    return <p>No feedback given</p>
+    return <h4>No feedback given</h4>
   }
 
-  return <div>
+  return <table><tbody>
       <StatisticLine value={good} text="good"/>
       <StatisticLine value={neutral} text="neutral"/>
       <StatisticLine value={bad} text="bad"/>
       <StatisticLine value={total} text="total"/>
       <StatisticLine value={average} text="average"/>
       <StatisticLine value={positive+"%"} text="positive"/>
-  </div>
+  </tbody></table>
 }
 const App=()=> {
   const [good,setGood]=useState(0)
