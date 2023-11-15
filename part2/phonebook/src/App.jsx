4c91b1e0cf68c8101b0982609f44bb9eca682fa2
hamini <ahmademin321@gmail.com>
2023-11-15 17:16:48 +0300

    Exercise 2.11 Done.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue,setFilterValue] = useState('')
   const [filter,setFilter] = useState(true)
+
+  useEffect(()=>{
+    axios.get('http://localhost:3002/persons').then(promise=>{
+      setPersons(promise.data)
+      console.log('Done from effect')
+    })
+  },[])
 
   const allPersons=filter ? persons : persons.filter(item=>item.name.toLowerCase().includes(filterValue.toLowerCase()))
 
