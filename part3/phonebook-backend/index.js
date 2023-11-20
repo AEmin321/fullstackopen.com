8b316556ee2f135c69be07c8efa95ffef24f8149
hamini <ahmademin321@gmail.com>
2023-11-21 00:25:18 +0300

    Exercise 3.8 Done.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use (express.json())
-app.use (morgan('tiny'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
+
 
 let persons=[
     { 
