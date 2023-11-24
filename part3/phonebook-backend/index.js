0c23230a69bc08953fa5c717b1bc8df43677550c
hamini <ahmademin321@gmail.com>
2023-11-24 15:09:38 +0300

    Exercise 3.13 Done.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+require('dotenv').config()
+const Contact = require('./modules/contact')
 const app = express()
 app.use (express.json())
 app.use (express.static('dist'))
---
+++ part3/phonebook-backend/modules/contact.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.PHONEBOOK_URL
+console.log('Connecting to the URL:',url)
+
+mongoose.connect(url).then(res=>{
+    console.log('Connected to mongoDB...')
+}).catch(error=>{
+    console.log('An error occured while connecting to mongoDB:',error.message)
+})
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+contactSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Contact',contactSchema)
