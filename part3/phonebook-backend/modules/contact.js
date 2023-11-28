5c0cf69ff1fff9ea78cf32ea8b13196084c951ae
hamini <ahmademin321@gmail.com>
2023-11-28 23:04:30 +0300

    Exercise 3.20 Done

--- part3/phonebook-backend/modules/contact.js
+++ part3/phonebook-backend/modules/contact.js
@@ -18,7 +18,17 @@ const contactSchema = new mongoose.Schema({
         minLength:3,
         required:true
     },
-    number: String
+    number: {
+        type:String,
+        validate:{
+            validator:function(v){
+                return /\d{3}-\d{3}\d{4}/.test(v);
+            },
+            message:props=>`${props.value} is not a valid phone number!`
+        },
+        required:[true,'Phone number is required'],
+        minLength:8
+    }
 })
 
 contactSchema.set('toJSON', {
