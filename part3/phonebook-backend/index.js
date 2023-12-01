643282146b735903dc00f04b25de653f4a2bbc32
hamini <ahmademin321@gmail.com>
2023-12-01 19:36:20 +0300

    Exercise 3.22 Done.

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,97 +5,69 @@ const Contact = require('./modules/contact')
 const app = express()
 
 const unknownEndPoints = (req,res)=> {
-  res.status(404).send({error:'Unknown end point.'})
+	res.status(404).send({error:'Unknown end point.'})
 }
 
 const errorHandler = (error,req,res,next) =>{
-  console.error(error.message)
-  if (error.name==='CastError') {
-    return res.status(400).send({error:'input id is not valid.'})
-  }else if (error.name==='ValidationError'){
-    return res.status(400).send({error:error.message})
-  }
+	console.error(error.message,next)
+	if (error.name==='CastError') {
+		return res.status(400).send({error:'input id is not valid.'})
+	}else if (error.name==='ValidationError'){
+		return res.status(400).send({error:error.message})
+	}
 }
 
 app.use (express.json())
 app.use (express.static('dist'))
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
-
-
-let persons=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const randomID = () => {
-  return Math.floor(Math.random()*500)+1
-}
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
 app.post ('/api/persons',(req,res,next)=>{
-  const data = req.body
-  if (data.name===undefined || data.number===undefined){
-    return res.status(400).json({error:'Name or the number is missing.'})
-  }
-  const newContact = new Contact({
-    name: data.name,
-    number: data.number
-  })
-  newContact.save().then(response=>{
-    res.json(response)
-  }).catch(error=>next(error))
+	const data = req.body
+	if (data.name===undefined || data.number===undefined){
+		return res.status(400).json({error:'Name or the number is missing.'})
+	}
+	const newContact = new Contact({
+		name: data.name,
+		number: data.number
+	})
+	newContact.save().then(response=>{
+		res.json(response)
+	}).catch(error=>next(error))
 })
 
 app.get ('/api/persons',(req,res)=>{
-    Contact.find({}).then(response=>{
-      res.json(response)
-    })
+	Contact.find({}).then(response=>{
+		res.json(response)
+	})
 })
 
 app.get ('/api/info',(req,res)=>{
-    const now = new Date()
-    const getDate = now.toLocaleString()
-    Contact.find({}).count().then(response=>{
-      res.send(`<div><h3>Phonebook had info for ${response} people.</h3><p>${getDate}</p></div>`)
-    })
+	const now = new Date()
+	const getDate = now.toLocaleString()
+	Contact.find({}).count().then(response=>{
+		res.send(`<div><h3>Phonebook had info for ${response} people.</h3><p>${getDate}</p></div>`)
+	})
 })
 
 app.delete ('/api/persons/:id',(req,res,next)=>{
-  Contact.findByIdAndDelete(req.params.id).then(response=>{
-    res.status(204).end()
-  }).catch(error=>next(error))
+	Contact.findByIdAndDelete(req.params.id).then(()=>{
+		res.status(204).end()
+	}).catch(error=>next(error))
 })
 
 app.put ('/api/persons/:id',(req,res,next)=>{
-  const {name,number}=req.body
-  Contact.findByIdAndUpdate(req.params.id,{name,number},{new:true,runValidators:true,context:'query'}).then(response=>{
-    res.json(response)
-  }).catch(error=>next(error))
+	const {name,number}=req.body
+	Contact.findByIdAndUpdate(req.params.id,{name,number},{new:true,runValidators:true,context:'query'}).then(response=>{
+		res.json(response)
+	}).catch(error=>next(error))
 })
 
 app.get ('/api/persons/:id',(req,res,next)=>{
-    Contact.findById(req.params.id).then(response=>{
-      res.json(response)
-    }).catch(error=>next(error))
+	Contact.findById(req.params.id).then(response=>{
+		res.json(response)
+	}).catch(error=>next(error))
 })
 
 app.use(unknownEndPoints)
@@ -103,5 +75,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT||3002
 app.listen (PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
+	console.log(`Server is running on port ${PORT}`)
 })
--- part3/phonebook-backend/modules/contact.js
+++ part3/phonebook-backend/modules/contact.js
@@ -6,37 +6,37 @@ mongoose.set('strictQuery',false)
 const url = process.env.MONGODB_URI
 console.log('Connecting to the URL:',url)
 
-mongoose.connect(url).then(res=>{
-    console.log('Connected to mongoDB...')
+mongoose.connect(url).then(()=>{
+	console.log('Connected to mongoDB...')
 }).catch(error=>{
-    console.log('An error occured while connecting to mongoDB:',error.message)
+	console.log('An error occured while connecting to mongoDB:',error.message)
 })
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        minLength:3,
-        required:true
-    },
-    number: {
-        type:String,
-        validate:{
-            validator:function(v){
-                return /\d{3}-\d{3}\d{4}/.test(v);
-            },
-            message:props=>`${props.value} is not a valid phone number!`
-        },
-        required:[true,'Phone number is required'],
-        minLength:8
-    }
+	name: {
+		type:String,
+		minLength:3,
+		required:true
+	},
+	number: {
+		type:String,
+		validate:{
+			validator:function(v){
+				return /\d{3}-\d{3}\d{4}/.test(v)
+			},
+			message:props=>`${props.value} is not a valid phone number!`
+		},
+		required:[true,'Phone number is required'],
+		minLength:8
+	}
 })
 
 contactSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Contact',contactSchema)
