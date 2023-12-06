379a34f837aa2999912a9f05eafe9d659b91fc1c
hamini <ahmademin321@gmail.com>
2023-12-06 20:15:32 +0300

    Exercise 4.5 Done.

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,6 +6,11 @@ const totalLikes = (blogs) => {
     return blogs.reduce((total,item) => total+item.likes,0)
 }
 
+const fevoriteBlog = (blogs)=> {
+    const theObj= blogs.reduce((maxObject,currentObject)=>maxObject.likes>currentObject.likes?maxObject:currentObject,blogs[0])
+    return theObj
+}
+
 module.exports = {
-    dummy,totalLikes
+    dummy,totalLikes,fevoriteBlog
 }
