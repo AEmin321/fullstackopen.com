3782e5ee2be4c329f007e87edd5022ad7ea0b65a
hamini <ahmademin321@gmail.com>
2023-12-23 22:32:32 +0300

    Exercise 4.15 Done.

---
+++ part4/controllers/users.js
@@ -0,0 +1,25 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+usersRouter.post('/', async(request,response)=>{
+    const {username,name,password} = request.body
+    const saltRound = 10
+    const hashPassword = await bcrypt.hash(password,saltRound)
+
+    const user = new User({
+        username:username,
+        name:name,
+        passwordHash:hashPassword
+    })
+
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async(request,response)=>{
+    const getUsers = await User.find({})
+    response.json(getUsers)
+})
+
+module.exports = usersRouter
