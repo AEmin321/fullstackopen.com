2554b5ced12efcf6da14886c7327e0661f5562a4
hamini <ahmademin321@gmail.com>
2024-01-02 19:23:36 +0300

    Exercise 5.1 Done

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username,setUserName] = useState('')
+  const [password,setPassword] = useState('')
+  const [user,setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +15,44 @@ const App = () => {
     )  
   }, [])
 
-  return (
+  const handleLogin = async (event)=>{
+    event.preventDefault()
+    try {
+      const user = await loginService.login({username:username,password:password})
+      setUserName('')
+      setPassword('')
+      setUser(user)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const login = () => (
     <div>
-      <h2>blogs</h2>
+      <h2>login to application</h2>
+      <form onSubmit={handleLogin}>
+        <label htmlFor="Username">Username:</label><input type="text" name='Username' value={username} onChange={({target})=>setUserName(target.value)} /><br/>
+        <label htmlFor="Password">Password:</label><input type="password" name='Password' value={password} onChange={({target})=>setPassword(target.value)}/>
+        <div>
+          <button type='submit'>Login</button>
+        </div>
+      </form>
+    </div>
+  )
+
+  const renderBlogs = () => (
+    <div>
+      <h2>Blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+    </div>
+  )
+
+  return (
+    <div>
+      {!user && login()}
+      {user && <div>{user.name}<p> is logged in</p> {renderBlogs()}</div>}
     </div>
   )
 }
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
+
+const baseUrl = 'http://localhost:3002/api/blogs'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
