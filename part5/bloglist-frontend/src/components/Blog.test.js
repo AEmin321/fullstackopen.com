e0be4f791767aba0f01cf61df33a2ac4e15b6f56
hamini <ahmademin321@gmail.com>
2024-01-07 01:14:08 +0300

    Exercise 5.15 Done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -4,22 +4,22 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const blog = {
+  title:'sampleblog',
+  author:'mahmut',
+  url:'https/:newomoo/com/slj',
+  likes:34
+}
+const username = {
+  name:'random',
+  username:'random2'
+}
 
 describe('<Blog /> Tests',() => {
-  const blog = {
-    title:'sampleblog',
-    author:'mahmut',
-    url:'https/:newomoo/com/slj',
-    likes:34
-  }
-  const user = {
-    name:'random',
-    username:'random2'
-  }
   let container
 
   beforeEach(() => {
-    container = render(<Blog blog={blog} user={user} />).container
+    container = render(<Blog blog={blog} user={username} />).container
   })
 
   test('At the start it only shows author and title.',() => {
@@ -35,4 +35,16 @@ describe('<Blog /> Tests',() => {
     const viewBlog = container.querySelector('.viewBlog')
     expect(viewBlog).not.toHaveStyle('display:none')
   })
+})
+
+test('Clicking the like button increases the like and calls event handler', async() => {
+  const mockHandler = jest.fn()
+  render(<Blog blog={blog} user={username} handleLike={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('like')
+  await user.click(button)
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
