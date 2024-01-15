e22c3b42037e133fc69ce753202776b0b164c11b
hamini <ahmademin321@gmail.com>
2024-01-15 21:53:28 +0300

    Exercise 5.18 Done.

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog App', function(){
   beforeEach(function(){
     cy.request('POST','http://localhost:3002/api/testing/reset')
+    const user = {
+      username:'testing',
+      name:'theTEst',
+      password:'testing'
+    }
+    cy.request('POST','http://localhost:3002/api/users/', user)
     cy.visit('http://localhost:5173')
   })
   it('login form is shown', function(){
@@ -9,5 +15,20 @@ describe('Blog App', function(){
     cy.get('input[name="Username"]').should('exist')
     cy.get('input[name="Password"]').should('exist')
     cy.get('button[type="submit"]').should('exist')
+  })
+
+  describe('Login', function(){
+    it('Succeeds using correct username and password', function(){
+      cy.get('input[name="Username"]').type('testing')
+      cy.get('input[name="Password"]').type('testing')
+      cy.contains('Login').click()
+      cy.contains('theTEst is logged in')
+    })
+    it('Faild login using wrong username or password', function(){
+      cy.get('input[name="Username"]').type('testing')
+      cy.get('input[name="Password"]').type('wrong')
+      cy.contains('Login').click()
+      cy.contains('Wrong user name or password')
+    })
   })
 })
